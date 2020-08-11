0020a0b1bde02b7ca02369b22266ad3273bc1e8b
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-08-10 22:08:10 -0400

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -6,6 +6,7 @@ class MusicLibraryController
     
     def call
       user_input = gets.chomp
+      user_input != exit
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -15,5 +16,30 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
+    end
+
+    def list_songs
+        Song.all.sort {|a, b| a.name <=> b.name}.each_with_index do |song, i|
+            puts "#{i + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+        end
+    end
+
+    def list_artists
+        Artist.all.sort {|a, b| a.name <=> b.name}.each_with_index do |artist, i|
+            puts "#{i + 1}. #{artist.name}"
+        end
+    end
+
+    def list_genres
+        Genre.all.sort {|a, b| a.name <=> b.name}.each_with_index do |genre, i|
+            puts "#{i + 1}. #{genre.name}"
+        end
+    end
+
+    def list_songs_by_artist
+        puts "Please enter the name of an artist:"
+        user_input = gets.chomp
+
+        
     end
   end
