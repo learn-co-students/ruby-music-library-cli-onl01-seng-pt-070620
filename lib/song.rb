5b1f5da5c5aaaa7ab7af260574a206cd9155576d
Jill Short <jillian.b.short@gmail.com>
2020-08-13 18:33:41 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+require "pry"
+
+class Song 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    self.class.all << self
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def self.create(song)
+    song = self.new(song)
+    song.save
+    song
+  end
+  
+end
