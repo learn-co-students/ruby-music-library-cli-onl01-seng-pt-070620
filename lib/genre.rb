944604b29e9d83a697df98dae55258a7ba848054
Jill Short <jillian.b.short@gmail.com>
2020-08-13 21:45:35 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,19 @@
+require_relative "./song.rb"
+require "pry"
+
 class Genre < Song
+  extend Concerns::Findable
+  
+  def artists 
+    @artist_collection = []
+    @songs.collect do |song|
+      if @artist_collection.include?(song.artist)
+        nil
+      else
+        @artist_collection << song.artist 
+      end
+    end
+    @artist_collection
+  end
   
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,15 @@
 require "pry"
 
 class Song 
-  attr_accessor :name, :songs, :artist 
+  attr_accessor :name, :songs, :artist, :genre
   
   @@all = []
   
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
     @songs = []
     self.artist = (artist) if artist !=nil
+    self.genre = (genre) if genre !=nil
   end
   
   def self.all
@@ -28,5 +29,31 @@ class Song
     song.save
     song
   end
+  
+  def artist
+    @artist
+  end
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+  
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end 
   
 end
