24c40a4d450825b48f155a93dcd27b275f47a0d0
Katie Le <lethikatie@gmail.com>
2020-08-14 19:50:18 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song 
   attr_accessor :name, :artist, :genre, :musicimporter, :musiclibrarycontroller 
   
-  extend Findable::ClassMethods 
+  extend Concerns::Findable 
   @@all = []
   
   def initialize(name, artist = nil, genre = nil)
