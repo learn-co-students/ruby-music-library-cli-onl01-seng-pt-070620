7c950dfde427587cfe87f2004b5a9c2b61525477
Katie Le <lethikatie@gmail.com>
2020-08-14 19:35:51 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+class Artist 
+  attr_accessor :name, :song, :musiclibrarycontroller, :musicimporter 
+  @@all = []
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.destroy_all 
+    @@all.clear 
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.create(artist)
+    artist = self.new(artist)
+    artist.save 
+    artist 
+  end 
+  
+  def songs 
+    @songs 
+  end 
+  
+  def add_song(song) 
+    song.artist = self unless song.artist 
+    songs << song unless songs.include?(song)
+  end 
+  
+  def genres
+    songs.collect {|song|song.genre}.uniq
+  end 
+end 
