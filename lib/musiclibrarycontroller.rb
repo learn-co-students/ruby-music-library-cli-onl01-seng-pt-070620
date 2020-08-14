50e1e012fa5b5e8ddfb733a2895a0c8f7ee25317
Jill Short <jillian.b.short@gmail.com>
2020-08-14 00:16:22 +0000

    Done.

---
+++ lib/musiclibrarycontroller.rb
@@ -0,0 +1,11 @@
+class MusicLibraryController
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def call 
+    
+  end
+  
+end
--- lib/song.rb
+++ lib/song.rb
@@ -56,4 +56,18 @@ class Song
     self.find_by_name(name) || self.create(name)
   end 
   
+  def self.new_from_filename(filename)
+    song_name = filename.split(" - ")[1]
+    artist_name = filename.split(" - ")[0]
+    genre_name = filename.split(" - ")[2].split(".mp3").join
+    
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.new(song_name, artist, genre)
+  end
+  
+  def self.create_from_filename(filename)
+    new_from_filename(filename).save
+  end
+  
 end
