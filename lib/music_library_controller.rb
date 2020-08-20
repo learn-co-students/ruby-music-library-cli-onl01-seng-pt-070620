c1a374a8b0c578e5dbbf2e6e761ba7015160f120
Jbeltrez <beltrezjohn@gmail.com>
2020-08-20 19:14:30 -0400

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,13 +1,27 @@
+#require_relative "../config/environment.rb"
 class MusicLibraryController 
     def initialize(path= "./db/mp3s")
         mi = MusicImporter.new(path)
         mi.import
     end
     def call
+        input = gets.strip 
+        if input != exit 
 
+        puts "Welcome to your music library!"
+        puts "To list all of your songs, enter 'list songs'."
+        puts "To list all of the artists in your library, enter 'list artists'."
+        puts "To list all of the genres in your library, enter 'list genres'."
+        puts "To list all of the songs by a particular artist, enter 'list artist'."
+        puts "To list all of the songs of a particular genre, enter 'list genre'."
+        puts "To play a song, enter 'play song'."
+        puts "To quit, type 'exit'."
+        puts "What would you like to do?"
+        end 
         def list_songs 
             Song.all.sort {|a,b| a.name <=> b.name}.each_with_index do |s,i| 
-                puts " #{i+1}. #{s.name} - #{s.artist} - #{s.genre}"
+                puts " #{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+            end
         end 
     end 
 end 
